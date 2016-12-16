d8bebfd3af181ba64cc79d6290b02fdde8505b3f
Adam Reese <adam@reese.io>
2016-12-16 13:51:41 -0800

    add support for tiller namespaces

--- tiller/tiller.sh
+++ tiller/tiller.sh
@@ -16,24 +16,26 @@ Available Commands:
 EOF
 }
 
+TILLER_NAMESPACE=${TILLER_NAMESPACE:-kube-system}
+
 pod_name() {
-  kubectl -n kube-system get pod --selector=name=tiller,app=helm -o jsonpath='{.items..metadata.name}'; echo
+  kubectl -n "${TILLER_NAMESPACE}" get pod --selector=name=tiller,app=helm -o jsonpath='{.items..metadata.name}'; echo
 }
 
 image() {
-  kubectl -n kube-system get pod "$(pod_name)" -o jsonpath='{.spec.containers[0].image}'; echo
+  kubectl -n "${TILLER_NAMESPACE}" get pod "$(pod_name)" -o jsonpath='{.spec.containers[0].image}'; echo
 }
 
 describe() {
-  kubectl -n kube-system describe pod "$(pod_name)"
+  kubectl -n "${TILLER_NAMESPACE}" describe pod "$(pod_name)"
 }
 
 logs() {
-  kubectl -n kube-system logs "$(pod_name)" "$@"
+  kubectl -n "${TILLER_NAMESPACE}" logs "$(pod_name)" "$@"
 }
 
 destroy() {
-  kubectl -n kube-system delete deployment tiller-deploy
+  kubectl -n "${TILLER_NAMESPACE}" delete deployment tiller-deploy
 }
 
 cmd="${1:-}"
